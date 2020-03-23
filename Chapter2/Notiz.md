0d6386c58cb9770a611d5d4328f1f2d8198af14e
Morgen <anchroom@gmail.com>
2020-03-23 22:18:47 +0100

    P2.10 ends

--- Chapter2/Notiz.md
+++ Chapter2/Notiz.md
@@ -15,3 +15,18 @@ Scanner input = new Scanner (System.in);
 
 3.String 不是一个基本类型，而是一个reference type
 
+4.输入输出显示框
+```java
+//把所有输入先都读成String
+String input = JOptionPane.showInputDialog(null, "Enter a input", "Input Dialog Demo", JOptionPane.QUESTION_MESSAGE);
+//然后运用对应数据类型的转换方法
+//如int，如字符串为123,则轮换为123
+int intValue = Integer.parseInt(intString);
+//如 double,如字符串
+double doubleValue = Double.parseDouble(doubleString);
+
+//输出通过语句
+String output = "The result you want to display";
+JOptionPane.showMessageDialog(null, output);
+```
+
--- Chapter2/src/P2_10.java
+++ Chapter2/src/P2_10.java
@@ -1,5 +1,45 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class P2_10 {
     public static void main(String[] args){
-        System.out.println("There is a error in the question");
+        // Create a window for input
+        String input = JOptionPane.showInputDialog(null,
+                "Enter a mount in int form, for example 1156, last two digits for 0.56.",
+                "Money change",
+                JOptionPane.QUESTION_MESSAGE);
+
+        // transfer input from String to int
+        int amount = Integer.parseInt(input);
+
+        int remainingAmount = amount;// a variable for transfer values
+
+        // find the number of one dollars
+        int numberOfDollars = amount/ 100;
+        remainingAmount = remainingAmount % 100;
+
+        // find the number of quarters in the reminning amount
+        int numberOfQuarters = remainingAmount /25;
+        remainingAmount = remainingAmount % 25;
+
+        // find the number of dimes in the remaining amount
+        int numberOfdimes = remainingAmount / 10;
+        remainingAmount = remainingAmount % 10;
+
+        // find the number of nickels in the remaining amount
+        int numberOfNickels = remainingAmount / 5;
+        remainingAmount = remainingAmount % 5;
+
+        // find the number of pennies in the remaining amount
+        int numberOfPennies = remainingAmount;
+
+        // Display results
+        String output = "Your amount " + amount + " consists of \n" +
+                "\t" + numberOfDollars + " dollars\n" +
+                "\t" + numberOfQuarters + " quarters\n" +
+                "\t" + numberOfdimes + " dimes\n" +
+                "\t" + numberOfNickels + " nickels\n" +
+                "\t" + numberOfPennies + " pennies\n" ;
+        JOptionPane.showMessageDialog(null,output);
     }
 }
